223bbacf7deb7b20089e1e0e489411620ac4b57d
Usyk <milausik@ukr.net>
2021-06-06 23:50:28 +0300

    added 3 module

---
+++ hw_3.js
@@ -0,0 +1,60 @@
+//1
+// const apartment = {
+//     imgUrl: "https://via.placeholder.com/640x480",
+//     descr:  "Spacious apartment in the city center",
+//     rating: 4,
+//     price: 2153,
+//     tags: ["premium", "promoted", "top"],
+//     };
+
+
+//2
+// const apartment = {
+//     imgUrl: 'https://via.placeholder.com/640x480',
+//     descr: 'Spacious apartment in the city center',
+//     rating: 4,
+//     price: 2153,
+//     tags: ['premium', 'promoted', 'top'],
+//   owner: {
+//   	name: "Henry",
+//   	phone: "982-126-1588",
+//   	email: "henry.carter@aptmail.com",}
+//   };
+
+
+//3
+// const apartment = {
+//     imgUrl: "https://via.placeholder.com/640x480",
+//     descr: "Spacious apartment in the city center",
+//     rating: 4,
+//     price: 2153,
+//     tags: ["premium", "promoted", "top"],
+//   };
+//   const aptRating = apartment.rating;
+//   const aptDescr = apartment.descr;
+//   const aptPrice = apartment.price;
+//   const aptTags = apartment.tags;
+//console.log(apartment);
+
+
+//4
+// const apartment = {
+//     imgUrl: "https://via.placeholder.com/640x480",
+//      descr: "Spacious apartment in the city center",
+//      rating: 4,
+//      price: 2153,
+//      tags: ["premium", "promoted", "top"],
+//      owner: {
+//       name: "Henry",
+//       phone: "982-126-1588",
+//       email: "henry.carter@aptmail.com",
+//      }};
+//      const ownerName = apartment.owner.name;
+//      const ownerPhone = apartment.owner.phone;
+//      const ownerEmail = apartment.owner.email;
+//      const numberOfTags = apartment.tags.length;
+//      const firstTag = apartment.tags[0];
+//      const lastTag = apartment.tags[2];
+
+
+//5
